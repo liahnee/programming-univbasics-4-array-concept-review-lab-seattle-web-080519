c56df4700761c7833db544a4d3824ec6c708c713
Catherine Yang <liahnee@gmail.com>
2019-07-17 04:19:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,8 +22,8 @@ def find_min_value(array)
   min = array[0]
   array.length.times {|index|
     if array[index] < min
-      min = array[index]
+      min_index = index
     end
   }
-  return nil
+  return index
 end
